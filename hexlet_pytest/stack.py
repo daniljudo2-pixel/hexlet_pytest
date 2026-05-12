4daf5d1ea2b79eaf3639ccd0bae9836a9d9de233
Zhuravlev Danila <daniljudo2@gmail.com>
2026-05-12 21:49:39 +0700

    add stack and test_stack

---
+++ hexlet_pytest/stack.py
@@ -0,0 +1,13 @@
+def stack():
+    stack_list = []
+    not stack
+
+    stack_list.append(1)
+    stack_list.append(2)
+    stack_list.append(3)
+    not stack
+
+    stack_list.pop()
+    stack_list.pop()
+    stack_list.pop()
+    not stack
